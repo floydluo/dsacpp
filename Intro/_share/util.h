2e27a96e4bd3d1ec87f794c2a4e8a35161b971c3
Floyd Luo <114020252@link.cuhk.edu.cn>
2018-01-07 00:43:49 +0800

    tools for test

---
+++ Intro/_share/util.h
@@ -0,0 +1,32 @@
+﻿
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+/******************************************************************************************
+ * 测试、演示小工具
+ ******************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <typeinfo.h>
+
+#include "comparator.h"
+#include "checkOrder_Elem.h"
+
+#include "double_Elem.h"
+#include "increase_Elem.h"
+#include "hailstone_Elem.h"
+
+#include "crc_Elem.h"
+
+#include "rand.h" //
+
+#if defined(DSA_DEBUG) //
+#define DSA(x) { x } //
+#else
+#define DSA(x) //
+#endif
