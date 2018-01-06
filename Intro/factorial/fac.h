08cd1837044d96beb562d5d2a32b9cc05ffaacfd
Floyd Luo <114020252@link.cuhk.edu.cn>
2018-01-07 00:40:34 +0800

    factorial

---
+++ Intro/factorial/fac.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdio.h>
+
+__int64 facI ( int );
+__int64 facR ( int );
---
+++ Intro/factorial/main.cpp
@@ -0,0 +1,9 @@
+﻿
+#include "fac.h"
+
+int main ( int argc, char* argv[] ) { 
+	//阶乘运算测试
+	for ( int i = 0; i < 10; i++ )
+		printf ( "\t%d\t%22I64d\t%22I64d\n", i, facI ( i ), facR ( i ) );
+	return 0;
+}
