868f564e5327120ae6f31608f08c0cd807d0f45b
Floyd Luo <114020252@link.cuhk.edu.cn>
2018-01-07 00:56:58 +0800

    server codes

---
+++ List/_share/checkOrder_Elem.h
@@ -0,0 +1,7 @@
+#pragma once
+
+template <typename T> struct CheckOrder {
+   T pred; int& u;
+   CheckOrder ( int& unsorted, T& first ) : pred ( first ), u ( unsorted ) { }
+   virtual void operator() ( T& e ) { if ( pred > e ) u++; pred = e; }
+};
